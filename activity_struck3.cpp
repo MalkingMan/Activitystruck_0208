918da9c0bd9d00847c9cf641e796251d83108052
MalkingMan <arrayalkhozini@gmail.com>
2024-04-02 14:01:53 +0700

    commit 12

--- activity_struck3.cpp
+++ activity_struck3.cpp
@@ -14,3 +14,11 @@ struct Mahasiswa
     DetailAlamat alamat; 
     int umur; 
 };
+
+int main()
+{
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout <<"Nama Mahasiswa : ";
+    cin >> mhs.nama;
