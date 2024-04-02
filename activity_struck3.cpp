5dbecf02b8057903e1c03116cf24d9e04b8d80aa
MalkingMan <arrayalkhozini@gmail.com>
2024-04-02 14:06:31 +0700

    commit 16

--- activity_struck3.cpp
+++ activity_struck3.cpp
@@ -17,6 +17,15 @@ struct Mahasiswa
 
 int main()
 {
+    Mahasiswa mhs[3];
+    for (int i=0;i<3;i++){
+    cout << "Nomor Mahasiswa : ";
+    getline(cin, mhs.nim);
+    cout <<"Nama Mahasiswa: ";
+    getline(cin,mhs.nama);
+
+
+
     Mahasiswa mhs;
     cout << "Nomor Mahasiswa : ";
     cin >> mhs.nim;
@@ -31,7 +40,7 @@ int main()
 
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
-
+    }
     cout << endl;
     cout <<"\n NIM: "<<mhs.nim;
     cout << "\n Nama: "<< mhs.nama;
@@ -39,3 +48,4 @@ int main()
     cout << "\n Desa: "<< mhs.alamat.desa;
     cout << "\n Kota: "<< mhs.alamat.kota;
     cout << "\n Umur: "<< mhs.umur;
+}
