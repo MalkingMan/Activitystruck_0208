7bccfaab664ba8b49060b0db3c2be6a5b55f53df
MalkingMan <arrayalkhozini@gmail.com>
2024-04-02 13:57:05 +0700

    commit 9

--- Activity_struck2.cpp
+++ Activity_struck2.cpp
@@ -30,4 +30,14 @@ int main()
     cout << "\t Nama Kota: ";
     cin >> mhs.alamat.kota;
     
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
     
+    cout << endl;
+    cout <<"\n NIM: "<<mhs.nim;
+    cout << "\n Nama: "<< mhs.nama;
+    cout << "\n Alamat: ";
+    cout << "\n Desa: "<< mhs.alamat.desa;
+    cout << "\n Kota: "<< mhs.alamat.kota;
+    cout << "\n Umur: "<< mhs.umur;
+}
